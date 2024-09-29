e7c983b4cd56b48d51e96af4bca8aff44b5fe0a4
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2024-09-30 00:08:56 +0200

    .

--- app/src/main/java/com/example/divisordecuentas/MainActivity.kt
+++ app/src/main/java/com/example/divisordecuentas/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.divisordecuentas
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Switch
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.slider.Slider
 import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
@@ -20,11 +24,32 @@ class MainActivity : AppCompatActivity() {
             insets
         }
         val button = findViewById<Button>(R.id.bttnCalcular)
-        button.setOnClickListener{
-            //.getText().toString();
-            val cuenta = findViewById<TextInputEditText>(R.id.numCuenta).text.toString()
-            val Nperso = findViewById<TextInputEditText>(R.id.numPersonas).text.toString()
-            Snackbar.make(button, "hola $cuenta , $Nperso", Snackbar.LENGTH_LONG).show()
+        val mySwitch = findViewById<SwitchMaterial>(R.id.swPropina)
+
+        mySwitch.setOnCheckedChangeListener { buttonView, isChecked ->
+            val numCuenta = findViewById<TextInputEditText>(R.id.numCuenta).text.toString()
+            val numC = numCuenta.toDoubleOrNull()
+            val numPerso = findViewById<TextInputEditText>(R.id.numPersonas).text.toString()
+            val numP = numPerso.toDoubleOrNull()
+            var cntTotal = findViewById<TextView>(R.id.cntTotal).text.toString()
+            var cntPersona = findViewById<TextView>(R.id.cntPersona).text.toString()
+            var slider = findViewById<Slider>(R.id.cntPersona).value
+            val total = numC!!.div(numP!!)
+            val propina = total.div(slider)
+            if (isChecked) {
+                button.setOnClickListener {
+                    Snackbar.make(button, "hola $numCuenta", Snackbar.LENGTH_LONG).show()
+                    val totalPropina = total + propina
+                    val totalPropinaPers = totalPropina.div(numPerso.toDouble())
+                    cntTotal = ("Total persona = $totalPropina")
+                    cntPersona = ("Total persona = $totalPropinaPers")
+                }// Acciones al estar activo
+            } else {
+                cntTotal = ("Total persona = $numCuenta")
+                cntPersona = ("Total persona = $total")
+
+            }
+
+        }
         }
     }
-}
